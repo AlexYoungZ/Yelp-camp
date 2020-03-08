00f94bf141b0ac3abe06501ca56850f7a8daef9e
AlexYoungZ <zhangsiy2015@163.com>
2020-03-08 17:14:10 +0800

    v3 complete

--- README.md
+++ README.md
@@ -49,3 +49,35 @@ INDEX   /dogs      GET   Display a list of all dog
 NEW     /dogs/new  GET   Displays form to make a new dog
 CREATE  /dogs      POST  Add new dog to DB
 SHOW    /dogs/:id  GET   Shows info about one dog
+
+#v3:
+
+#Refactor Mongoose Code
+* Create a models directory
+* Use module.exports
+* Require everything correctly!
+    
+#Add Seeds File
+* Add a seeds.js file
+* Run the seeds file every time the server starts
+
+#Add the Comment model!
+* Make our errors go away!
+* Display comments on campground show page
+
+#v4:
+
+#Comment New/Create
+* Discuss nested routes
+* Add the comment new and create routes
+* Add the new comment form
+
+#Style Show Page
+* Add sidebar to show page
+* Display comments nicely
+
+#v5:
+
+#Finish Styling Show Page
+* Add public directory
+* Add custom stylesheet
--- app.js
+++ app.js
@@ -1,36 +1,15 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
+    seedDB = require("./seeds");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp",{userNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true,  useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-// SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create(
-//      {
-//          name: "Granite Hill",
-//          image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
-//          description: "This is a huge granite hill, no bathrooms.  No water. Beautiful granite!"
-
-//      },
-//      function(err, campground){
-//       if(err){
-//           console.log(err);
-//       } else {
-//           console.log("NEWLY CREATED CAMPGROUND: ");
-//           console.log(campground);
-//       }
-//     });
+seedDB();
 
 app.get("/", function (req, res) {
   res.render("landing");
@@ -74,10 +53,11 @@ app.get("/campgrounds/new", function (req, res) {
 // SHOW - shows more info about one campground
 app.get("/campgrounds/:id", function (req, res) {
   //find the campground with provided ID
-  Campground.findById(req.params.id, function (err, foundCampground) {
+  Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
     if (err) {
       console.log(err);
     } else {
+      console.log(foundCampground);
       //render show template with that campground
       res.render("show",{campground: foundCampground});
     }
