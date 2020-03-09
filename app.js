faaccda91511edf4e97f034aebe2235086f7e343
AlexYoungZ <zhangsiy2015@163.com>
2020-03-09 17:00:48 +0800

    add edit & delete campgrounds / comments , authorization campgrounds / comments

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var express = require("express"),
     seedDB = require("./seeds"),
     passport = require("passport"),
     LocalStrategy = require("passport-local"),
-    User = require("./models/user");
+    User = require("./models/user"),
+    methodOverride = require("method-override");
+
 
 //requiring routes
 var commentRoutes = require("./routes/comments"),
@@ -18,6 +20,7 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true,  
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 // seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
